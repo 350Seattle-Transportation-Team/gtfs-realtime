6183f5d733a3ba1d6751356bcf2b3761b0f1c8e6
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2019-06-10 01:01:28 -0700

    write function to find adjacent shape points

--- route_shape_process/find_closest_route_point.py
+++ route_shape_process/find_closest_route_point.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-def closest_point_on_line(point, two_points_on_line):
+#Hmm, actually, we want this function to also return the scalar ratio t
+#such that C = A + t(B-A), i.e. t=(P-A).(B-A) / |B-A|^2,
+#because we need t to calculate the shape distance to the point C
+#using the shape distances to A and B.
+def project_onto_line(point, two_points_on_line):
     """
     Returns the projection of a point onto a line, i.e. the closest point on the line to the point.
 
@@ -8,8 +12,8 @@ def closest_point_on_line(point, two_points_on_line):
 
     C = A + (P-A).(B-A) * (B-A) / |B-A|^2,
 
-    where . is the dot product, * is scalar multiplication,
-    and |x|^2 is the squared Euclidean norm of the vector x.
+    where C is the projection (closest point to P), . is the dot product,
+    * is scalar multiplication, and |x|^2 is the squared Euclidean norm of the vector x.
 
     The calculation occurs in "affine coordinates" since the line is not assumed
     to go through the origin.
@@ -28,11 +32,33 @@ def closest_point_on_line(point, two_points_on_line):
 
     Returns
     -------
-    closest_point : numpy.array
-        The point on the line closest to the
+    closest_point : numpy.array (1D)
+        The point on the line closest to `point`.
+        The shape will be (1,d), the same as that of `point`.
     """
-    segment_start = two_points_on_line[0]
-    segment_end = two_points_on_line[1]
+    segment_start, segment_end = two_points_on_line
     direction = segment_end - segment_start
     projection = (point - segment_start).dot(direction) * direction / np.sum(direction**2)
-    return segment_start[0] + projection
+    return segment_start + projection
+
+#Still working on this...
+def find_adjacent_shape_point_data(shape_point_lat, shape_point_lat_lon, gtfs_shapes_df, shape_id):
+    """
+    Finds the 2 adjacent points to the specified shape point.
+    """
+    # mask = ((gtfs_shapes_df.shape_pt_lat==shape_point_lat)
+    #         & (gtfs_shapes_df.shape_pt_lon==shape_point_lat_lon)
+    #         & (gtfs_shapes_df.shape_id==shape_id))
+    #gtfs_shapes_df[gtfs_shapes_df]
+    point_seq_number = gtfs_shapes_df.loc[
+        (gtfs_shapes_df.shape_pt_lat==shape_point_lat)
+        & (gtfs_shapes_df.shape_pt_lon==shape_point_lat_lon)
+        & (gtfs_shapes_df.shape_id==shape_id)
+        ].shape_pt_sequence.values
+    # print(point_seq_number)
+    df = gtfs_shapes_df[
+                (gtfs_shapes_df.shape_id==shape_id)
+                & (np.abs(gtfs_shapes_df.shape_pt_sequence-point_seq_number)==1)
+                ]
+    # print(np.abs(gtfs_shapes_df.shape_pt_sequence-point_seq_number))
+    return df
