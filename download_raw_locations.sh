6c5d7317563b832e2112c1c9781c2835b8f81023
sidetrackedmind <malnorba@gmail.com>
2019-09-02 12:51:22 -0400

    fixed s3 location typo

--- download_raw_locations.sh
+++ download_raw_locations.sh
@@ -2,7 +2,7 @@
 
 pushd data/source
 #aws s3 folder structure unpacked/year/month/
-aws s3 cp s3:/bus350-data/unpacked/2019/05/ . --recursive --include '*'
+aws s3 cp s3://bus350-data/unpacked/2019/05/ . --recursive --include '*'
 unzip '*.zip'
 find * -type f -name '*csv' -exec mv {} . \;
 rmdir temp_*
